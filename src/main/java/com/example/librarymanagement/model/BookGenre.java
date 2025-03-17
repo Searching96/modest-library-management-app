aef62be0ed59c8a3c63e2439b20b99419dec03d9
Searching96 <nguyenphucthinh2k5@gmail.com>
2025-03-17 10:51:59 +0700

    Somthingg

--- src/main/java/com/example/librarymanagement/model/BookGenre.java
+++ src/main/java/com/example/librarymanagement/model/BookGenre.java
@@ -1,5 +1,53 @@
 package com.example.librarymanagement.model;
 
-public class BookGenre {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "book_genres", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"book_id", "genre_id"})
+})
+public class BookGenre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id", nullable = false)
+    private Genre genre;
+    
+    public BookGenre() {
+    }
+
+    public BookGenre(Book book, Genre genre) {
+        this.book = book;
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
 }
--- src/main/java/com/example/librarymanagement/model/Genre.java
+++ src/main/java/com/example/librarymanagement/model/Genre.java
@@ -1,5 +1,47 @@
 package com.example.librarymanagement.model;
 
-public class Genre {
+import jakarta.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String genreName;
+
+    @OneToMany(mappedBy = "genre", cascade = CascadeType.ALL)
+    private List<BookGenre> bookGenres;
+
+    public Genre() {
+    }
+
+    public Genre(String genreName) {
+        this.genreName = genreName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
+
+    public List<BookGenre> getBookGenres() {
+        return bookGenres;
+    }
+
+    public void setBookGenres(List<BookGenre> bookGenres) {
+        this.bookGenres = bookGenres;
+    }
 }
